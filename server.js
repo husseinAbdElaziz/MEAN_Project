1b6c4e1219b93a475460140901789daab2619a58
Hussein ELbadady <husseinabdelaziz8@gmail.com>
2019-03-13 22:14:59 +0200

    first commit

--- server.js
+++ server.js
@@ -6,15 +6,14 @@ const port = process.env.PORT || 8080;
 
 const app = express();
 app.use(cors())
+app.use(bodyParser.json()); 
+app.use('/api', api);
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
-  
-app.use(bodyParser.json()); 
-app.use('/api', api);
 
 app.listen(port, function(){
     console.log("Server running on localhost:" + port);
