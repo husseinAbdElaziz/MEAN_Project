00524133590adc78a5a81802fd0fb4f9a11cda6c
Hussein ELbadady <husseinabdelaziz8@gmail.com>
2019-03-13 21:27:18 +0200

    add CORS

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const api = require('./routes/api');
 const port = process.env.PORT || 8080;
 
 const app = express();
-app.use(cors())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use(bodyParser.json()); 
 app.use('/api', api);
 
@@ -13,8 +19,3 @@ app.listen(port, function(){
     console.log("Server running on localhost:" + port);
 });
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
