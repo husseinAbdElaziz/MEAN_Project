5e09c2de66c99389a6106fc73a709c3bb7245e35
HusseinElbdady <husseinabdelaziz8@gmail.com>
2019-08-02 15:43:57 +0200

    change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const api = require('./routes/api');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors({
